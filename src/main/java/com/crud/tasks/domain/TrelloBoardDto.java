83d6dddbefbb951c7d40ab9d8b211db3e8fc90e2
IgorSmolinski <pooky7@gmail.com>
2018-06-10 21:14:41 +0200

    18.2 zakomentowane

--- src/main/java/com/crud/tasks/domain/TrelloBoardDto.java
+++ src/main/java/com/crud/tasks/domain/TrelloBoardDto.java
@@ -1,4 +1,25 @@
 package com.crud.tasks.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.List;
+
+@AllArgsConstructor
+@Getter
+@JsonIgnoreProperties(ignoreUnknown=true)
 public class TrelloBoardDto {
+    @JsonProperty("name")
+    public String name;
+
+    @JsonProperty("id")
+    public String id;
+
+    @JsonProperty("closed")
+    public boolean isClosed;
+
+    @JsonProperty("lists")
+    public List<TrelloBoardDto> lists;
 }
--- src/main/java/com/crud/tasks/trello/client/TrelloClient.java
+++ src/main/java/com/crud/tasks/trello/client/TrelloClient.java
@@ -1,12 +1,19 @@
-package com.crud.tasks.treallo.client;
+package com.crud.tasks.trello.client;
 
 import com.crud.tasks.domain.TrelloBoardDto;
+import com.crud.tasks.domain.TrelloCardDto;
+import com.crud.tasks.mapper.CreatedTrelloCard;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 @Component
 public class TrelloClient {
@@ -14,20 +21,54 @@ public class TrelloClient {
     @Value("${trello.api.endpoint.prod}")
     private String trelloApiEndpoint;
 
-    @Value("${trello.api.key}")
+    @Value("${trello.app.key}")
     private String trelloAppKey;
 
-    @Value("${trello.api.token}")
+    @Value("${trello.app.token}")
     private String trelloToken;
+
+    @Value("${trello.app.username}")
+    private String trelloUserName;
 
 
     @Autowired
     private RestTemplate restTemplate;
 
-    public List<TrelloBoardDto> getTrelloBoards(){
+    private URI urlBuilder() {
 
-        TrelloBoardDto[] boardsResponse = restTemplate.getForObject(
-                "https://api.trello.com/1/members/pansmoa/boards",
-                TrelloBoardDto[].class);
+        URI url = UriComponentsBuilder.fromHttpUrl(trelloApiEndpoint + "/members/" + trelloUserName + "/boards")
+                .queryParam("key", trelloAppKey)
+                .queryParam("token", trelloToken)
+                .queryParam("fields", "name,id")
+                .queryParam("lists", "all").build().encode().toUri();
+
+        return url;
     }
+
+    public List<TrelloBoardDto> getTrelloBoards() {
+
+
+        TrelloBoardDto[] boardsResponse = restTemplate.getForObject(urlBuilder(), TrelloBoardDto[].class);
+
+
+        return   Optional.ofNullable(boardsResponse)
+                .map(Arrays::asList)
+                .orElse(new ArrayList<>());
+
+    }
+
+    public CreatedTrelloCard createNewCard(TrelloCardDto trelloCardDto){
+
+        URI url = UriComponentsBuilder.fromHttpUrl(trelloApiEndpoint + "/cards")
+                .queryParam("key", trelloAppKey)
+                .queryParam("token", trelloToken)
+                .queryParam("name", trelloCardDto.getName())
+                .queryParam("desc", trelloCardDto.getDescription())
+                .queryParam("pos", trelloCardDto.getPos())
+                .queryParam("idList", trelloCardDto.getListId()).build().encode().toUri();
+
+     return restTemplate.postForObject(url,null, CreatedTrelloCard.class);
+    }
+
+
 }
